4a4d9c367a31cc5923ed5b0369f8f459317d737d
Anid Murmu <anid.murmu@capfront.in>
2020-07-17 21:28:50 +0530

    in progress

--- app/src/main/java/in/example/cleanarchitecture/view/WordFragment.kt
+++ app/src/main/java/in/example/cleanarchitecture/view/WordFragment.kt
@@ -1,5 +1,6 @@
 package `in`.example.cleanarchitecture.view
 
+import `in`.example.cleanarchitecture.R
 import `in`.example.cleanarchitecture.base.DataBindingBaseFragment
 import `in`.example.cleanarchitecture.databinding.FragmentWordBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -7,6 +8,9 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class WordFragment: DataBindingBaseFragment<FragmentWordBinding>() {
 
   private val model: WordViewModel by viewModel()
+
+  override val layoutResource: Int
+    get() = R.layout.fragment_word
 
   override fun onViewDataBindingCreated(binding: FragmentWordBinding) {
     binding.viewModel = model
